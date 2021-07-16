e276795e329545a03269ff21554c03c52656226f
Sarbesh <sankarsarbesh@gmail.com>
2021-07-15 20:37:40 -0700

    Made code a little cleaner

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,54 +20,14 @@ driver.find_element_by_name('username').send_keys('pokevortex')
 driver.find_element_by_name('password').send_keys('azmarcos123')
 driver.find_element_by_name('Login').click()
 driver.implicitly_wait(10)
-'''for i in range(1000):
-
-    try:
-        element = WebDriverWait(driver, 1).until(
-            EC.presence_of_element_located((By.ID, "catch"))
-        )
-        element.click()
-    except:
-        try:
-            #if i%2 == 0:
-                element = WebDriverWait(driver, 2).until(
-                    EC.presence_of_element_located((By.ID, "move_n"))
-                )
-                element.click()
-            #else:
-                #element = WebDriverWait(driver, 2).until(
-                    #EC.presence_of_element_located((By.ID, "move_s"))
-                #)
-                #element.click()
-        except:
-            print('oof')
-    #try:
-        #element = WebDriverWait(driver, 2).until(
-        #EC.presence_of_element_located((By.ID, "move_s"))
-        #)
-       #element.click()
-    #except:
-        #print('no work')
-
-        try:
-        #element = WebDriverWait(driver, 2).until(
-            #EC.presence_of_element_located((By.CSS_SELECTOR, "input[class='btn-catch-button']"))
-        #)
-        #element.click()
-        driver.find_element_by_xpath('/html/body/div[3]/div[2]/div[4]/div[2]/div[1]/div[2]/form/input[4]').click()
-        print('goes in button')
-    except:
-        print('no work')
-'''
 j = 0
 for i in range(1000):
     while True:
-            #driver.find_element_by_id('catch').click()
             #Try to catch button
             try:
                 txt = driver.find_element_by_id('dexy').text
                 print(txt)
-                if 'Dark' in txt:
+                if 'Shiny' in txt or 'Chrome' in txt or 'Retro' in txt:
                     driver.implicitly_wait(5)
                     driver.find_element_by_class_name('btn-catch-action').click()
                     break
@@ -82,17 +41,6 @@ for i in range(1000):
                     # down arrow
                     driver.find_element_by_id('move_s').click()
                 print('fail')
-            '''if 'Normal' in driver.page_source:
-                driver.implicitly_wait(5)
-                driver.find_element_by_class_name('btn-catch-action').click()
-            else:
-                if j % 2 == 0:
-                #up arrow
-                    driver.find_element_by_id('move_n').click()
-                else:
-                #down arrow
-                    driver.find_element_by_id('move_s').click()
-                print('fail')'''
             j += 1
     driver.implicitly_wait(5)
     #scroll to bottom
@@ -112,19 +60,3 @@ for i in range(1000):
     driver.implicitly_wait(5)
     driver.back()
     driver.implicitly_wait(5)
-    '''
-    try:
-        driver.find_element_by_class_name('btn-battle-action').click()
-    except:
-        driver.find_element_by_class_name('btn-battle-action').click()
-
-    driver.implicitly_wait(5)
-    try:
-        driver.find_element_by_class_name('btn btn-primary').click()
-        #driver.find_element_by_link_text('Return To Map.')
-    except:
-        driver.find_element_by_class_name('btn btn-primary').click()
-        #driver.find_element_by_link_text('Return To Map.')
-    driver.implicitly_wait(2)
-    print('bruh did you click or not')
-    '''
